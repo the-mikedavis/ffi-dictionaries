a1e09372abe9d2296713d01087e07907926ac9c7
Michael Davis <mcarsondavis@gmail.com>
2024-08-31 12:39:38 -0400

    Support both dictionaries in the examples

--- examples/check.rs
+++ examples/check.rs
@@ -1,24 +1,62 @@
 use std::path::PathBuf;
 use std::time::Instant;
 
-use ffi_dictionaries::Nuspell;
+use ffi_dictionaries::{Hunspell, Nuspell};
+
+enum Dictionary {
+    Nuspell(Nuspell),
+    Hunspell(Hunspell),
+}
+
+impl Dictionary {
+    fn new(provider: &str) -> Self {
+        match provider {
+            "nuspell" => {
+                let manifest_path = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
+                let aff_path = manifest_path.join("vendor/en_US/en_US.aff");
+
+                let now = Instant::now();
+                let dict = Nuspell::new(aff_path.as_os_str());
+                println!("Compiled the dictionary in {}ms", now.elapsed().as_millis());
+                Self::Nuspell(dict)
+            }
+            "hunspell" => {
+                let manifest_path = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
+                let aff_path = manifest_path.join("vendor/en_US/en_US.aff");
+                let dic_path = manifest_path.join("vendor/en_US/en_US.dic");
+
+                let now = Instant::now();
+                let dict = Hunspell::new(aff_path.as_os_str(), dic_path.as_os_str());
+                println!("Compiled the dictionary in {}ms", now.elapsed().as_millis());
+                Self::Hunspell(dict)
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn spell(&self, word: &str) -> bool {
+        match self {
+            Self::Nuspell(dict) => dict.spell(word),
+            Self::Hunspell(dict) => dict.spell(word),
+        }
+    }
+}
 
 fn main() {
     let mut args = std::env::args().skip(1);
-    let word = match args.next() {
-        Some(arg) => arg,
-        None => {
-            eprintln!("Usage: check WORD");
+    let provider = match args.next() {
+        Some(arg) if arg == "hunspell" || arg == "nuspell" => arg,
+        _ => {
+            eprintln!("Usage: check {{nuspell|hunspell}} WORD");
             std::process::exit(1);
         }
     };
+    let Some(word) = args.next() else {
+        eprintln!("Usage: check {{nuspell|hunspell}} WORD");
+        std::process::exit(1);
+    };
 
-    let manifest_path = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let aff_path = manifest_path.join("vendor/en_US/en_US.aff");
-
-    let now = Instant::now();
-    let dict = Nuspell::new(aff_path.as_os_str());
-    println!("Compiled the dictionary in {}ms", now.elapsed().as_millis());
+    let dict = Dictionary::new(&provider);
 
     let now = Instant::now();
     if dict.spell(&word) {
